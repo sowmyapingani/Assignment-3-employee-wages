27ddac33cf1ae27b297eb9f516b947d127d9ac79
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-29 22:36:33 +0530

    Adde:message

---
+++ employeeWage.java
@@ -0,0 +1,8 @@
+package com.employeewages;
+
+public class employeeWage
+{
+    public static void main(String args[]){
+        System.out.println("Welcome to Employee Wages Computation");
+    }
+}

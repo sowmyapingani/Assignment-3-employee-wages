bf2fcedc6e4c2d0a8f5e2881c58d6abe7931bb21
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-30 00:08:22 +0530

    added:message

--- employeeWage.java
+++ employeeWage.java
@@ -1,8 +1,17 @@
 package com.employeewages;
 import java.util.Random;
-public class employeeWage
-{
+public class employeeWage {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wages Computation");
+        Random random = new Random();
+        int attendence = random.nextInt(2);
+        switch (attendence) {
+            case 0:
+                System.out.println("Employee is Absent");
+                break;
+            case 1:
+                System.out.println("Employee is present");
+                break;
+        }
     }
 }
